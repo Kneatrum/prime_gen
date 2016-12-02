038fa87bab2e5c1f826f0015214081ba14d2f723
kneatrum <martinmwiti1@yahoo.com>
2016-12-02 18:24:48 +0300

    The function should only accept numbers

--- Desktop/Sublime/generate_prime.py
+++ Desktop/Sublime/generate_prime.py
@@ -1,11 +1,15 @@
 
 def generate_prime(number):
-    for sample in range(number): # Loop through from (0) to (number)
-       
-       if sample > 1: # check if the sample is more than (1)
-           for x in range(2,sample): 
-               if (sample % x) == 0: # if a (sample) number is divisible by any other number apart from 1 and itelf
-                   break # break the loop
-           else:
-               print(sample)
-generate_prime(10)
+	if isinstance(number,int):
+	    for sample in range(number): # Loop through from (0) to (number)
+	       
+	       if sample > 1: # check if the sample is more than (1)
+	           for x in range(2,sample): 
+	               if (sample % x) == 0: # if a (sample) number is divisible by any other number apart from 1 and itelf
+	                   break # break the loop
+	           else:
+	               print(sample)
+	else:
+		print ("Please insert a number")
+
+generate_prime(15)
